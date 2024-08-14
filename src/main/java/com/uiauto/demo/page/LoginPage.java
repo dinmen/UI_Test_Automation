1e7d88c25330d084267508346a6cf1b5222e50f1
dinmen <164609145+dinmen@users.noreply.github.com>
2024-08-14 12:00:26 +0530

    Add files via upload

---
+++ src/main/java/com/uiauto/demo/page/LoginPage.java
@@ -0,0 +1,40 @@
+package com.uiauto.demo.page;
+
+import org.openqa.selenium.*;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage() {
+
+    }
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String userNameStr, String passwordStr) throws InterruptedException {
+        WebElement userName = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement submitButton = driver.findElement(By.id("login-button"));
+
+        userName.sendKeys(userNameStr);
+        Thread.sleep(1000);
+        password.sendKeys(passwordStr);
+        Thread.sleep(1000);
+        submitButton.click();
+    }
+
+    public boolean invalidLogin() {
+        try {
+            WebElement lblLoginErrorMsg = driver.findElement(By.className("error-message-container"));
+            if (lblLoginErrorMsg.isDisplayed()) {
+                return true;
+            }
+        } catch(Exception e) {
+            System.out.println("Exception thrown");
+        }
+
+        return false;
+    }
+
+}
